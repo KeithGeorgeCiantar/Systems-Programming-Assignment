de6ab5ce0829a29d43b70b9d72da0abde44a5190
keithgciantar <keith.g.ciantar.16@um.edu.mt>
2018-05-01 02:56:14 +0200

    All shell variables handled except for EXITCODE. Next step is to start interpreting input.

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <pwd.h>
+#include <sys/types.h>
 #include "linenoise.h"
 
 #define MAX_LENGTH 1024
 #define clear_terminal() printf("\033[H\033[J")
 #define TERMINAL_TITLE "Eggshell Terminal"
 
-static char* PATH = "/usr/bin";
+static char* PATH = "";
 static char* PROMPT = "eggsh> ";
-static char CWD[MAX_LENGTH];
-static char* USER;
-static char* HOME = "/home/keithgciantar";
-static char SHELL[MAX_LENGTH];
-static char TERMINAL[MAX_LENGTH];
+static char CWD[MAX_LENGTH] = "";
+static char* USER = "";
+static char* HOME = "";
+static char SHELL[MAX_LENGTH] = "";
+static char TERMINAL[MAX_LENGTH] = "";
 //static int EXTICODE = 1;
 
 void eggsh_start();
@@ -33,7 +35,11 @@ int main(int argc, char **argv, char **env) {
     eggsh_start();
     welcome_message();
 
-    change_directory("/home/keithgciantar/Desktop");
+    //just testing changing of directory
+    //change_directory("/home/keithgciantar/Desktop");
+
+    //add while to keep getting input
+    //make the prompt it's own function in a while loop
 
     input = linenoise(PROMPT);
 
@@ -45,34 +51,64 @@ int main(int argc, char **argv, char **env) {
 void eggsh_start(){
     print_header();
 
-    //get USER environmental variable
-    USER = getenv("USER");
+    static char* buffer;
+    size_t  buffer_size = (size_t)sysconf(_SC_GETPW_R_SIZE_MAX);
 
-    //get name of current terminal
-    if(ttyname_r(STDIN_FILENO, TERMINAL, MAX_LENGTH) != 0) {
-        perror("Cannot get terminal name");
+    if(buffer_size == -1){
+        buffer_size = 16*MAX_LENGTH;
     }
 
-    //get the current working directory
-    if(getcwd(CWD, MAX_LENGTH) == NULL) {
-        perror("Cannot get current working directory");
+    buffer = malloc(buffer_size);
+
+    struct passwd pwd;
+    struct passwd *user_details;
+    int out = getpwuid_r(getuid(), &pwd, buffer, buffer_size, &user_details);
+    if(user_details != NULL) {
+        if(out == 0) {
+            USER = user_details->pw_name;
+            HOME = user_details->pw_dir;
+        }
+    }
+
+    free(buffer);
+
+    //get the HOME environmental variable
+    if(getenv("HOME") != NULL && HOME != "") {
+        HOME = getenv("HOME");
+    }
+
+    //get the USER environmental variable
+    if(getenv("USER") != NULL && USER != "") {
+        USER = getenv("USER");
     }
 
     //get the working directory where the shell launched from
     if(getcwd(SHELL, MAX_LENGTH) == NULL) {
-        perror("Cannot get shell binary directory");
+        perror("Cannot get shell binary directory.\n");
+    }
+
+    //get the name of current terminal
+    if(ttyname_r(STDIN_FILENO, TERMINAL, MAX_LENGTH) != 0) {
+        perror("Cannot get terminal name.\n");
+    }
+
+    //get the current working directory
+    if(getcwd(CWD, MAX_LENGTH) == NULL) {
+        perror("Cannot get current working directory.\n");
     }
 
     //get the search path for external commands
-    PATH = getenv("PATH");
+    if(getenv("PATH") != NULL){
+        PATH = getenv("PATH");
+    }
 }
 
 void welcome_message(){
     printf("Welcome %s!\n", USER);
     printf("Terminal: %s\n", TERMINAL);
     printf("Working Directory: %s\n", CWD);
-    printf("Shell Path: %s\n\n", SHELL);
-    printf("Search Path: %s\n", PATH);
+    printf("Shell Path: %s\n", SHELL);
+    printf("Search Path: %s\n\n", PATH);
 }
 
 void change_directory(char* path){
